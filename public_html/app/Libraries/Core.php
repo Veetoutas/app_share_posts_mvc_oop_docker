7ef9bc956fc4cb823777da87b06f2db92fa4e7bd
Veetoutas <vyt.matulevicius@gmail.com>
2020-09-07 16:09:44 +0300

    Fixing few issues. Daily commit

--- public_html/app/Libraries/Core.php
+++ public_html/app/Libraries/Core.php
@@ -24,30 +24,19 @@ class Core
 
     public function __construct()
     {
+        // Get the URL
         $url = $this->getUrl();
         $urlShifted = array_shift($url);
-        // Look in Controllers for first value of URL
-        if (file_exists(ROOT_DIR . self::CONTROLLERS_DIR . ucwords($url[0]) . '.php')) {
-            // If exists, set it as controller
-            $this->currentController = ucwords($url[0]);
-            // Unset 0 Index
-            unset($url[0]);
-        }
-        // Require the controller
+        // Set Controller based on URL
+        $url = $this->setController($url);
+        // Name the Class to which the URL is targeting
         $className = 'VFramework\\Controllers\\' . ucfirst($this->currentController);
+        // Instantiate the class
         $this->currentController = new $className(
             new Validator(new User())
         );
-
-        // Check for second part of URL
-        if (isset($url[1])) {
-            // Check to see if method exists in controller
-            if (method_exists($this->currentController, $url[1])) {
-                $this->currentMethod = $url[1];
-                // Unset 1 index
-                unset($url[1]);
-            }
-        }
+        // Set the Method based on URL
+        $url = $this->setMethod($url);
         // Get params
         $this->params = $url ? array_values($url) : [];
         // Call a callback with array of params
@@ -61,16 +50,47 @@ class Core
     {
         // $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
         if (isset($_SERVER['REQUEST_URI'])) {
-
             // rtrim() Removes the desired char from the right side
             $url = rtrim($_SERVER['REQUEST_URI'], '/');
-
             // Sanitizes URL so that only URL chars remain
             $url = filter_var($url, FILTER_SANITIZE_URL);
-
             // Separate URL's parameters in array by '/'
             $url = explode('/', $url);
             return $url;
-        };
+        }
+    }
+
+    /**
+     * @param array $url
+     * @return array
+     */
+    public function setController(array $url): array
+    {
+        if (file_exists(ROOT_DIR . self::CONTROLLERS_DIR . ucwords($url[0]) . '.php')) {
+            // If exists, set it as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 Index
+            unset($url[0]);
+            return $url;
+        }
+        return [];
+    }
+
+    /**
+     * @param array $url
+     * @return array
+     */
+    public function setMethod(array $url): array
+    {
+        if (isset($url[1])) {
+            // Check to see if method exists in controller
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                // Unset 1 index
+                unset($url[1]);
+                return $url;
+            }
+        }
+        return [];
     }
 }
